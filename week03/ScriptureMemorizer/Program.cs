3ba606913c49086115c2401c779aef34f8f35444
Jaures Guedou <sewlanguedou@gmail.com>
2026-05-24 02:04:51 +0100

    I submit the Scripture Memorizer

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,5 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+
+        Reference reference = new Reference("Abraham", 2, 2);
+
+        Scripture scripture = new Scripture(
+            reference,
+            "And it came to pass that I, Abraham, took Sarai to wife, and Nahor, my brother, took Milcah to wife, who was the daughter of Haran."
+        );
+
+        while(!scripture.IsCompletelyHidden())
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+
+            Console.WriteLine("Press Enter to continue or type quit");
+
+            string input = Console.ReadLine();
+
+            if (input == "quit" || input == "Quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(4);
+
+        }
+
+        
+       Console.Clear();
+       Console.WriteLine(scripture.GetDisplayText());
+
+
     }
 }
--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,11 +1,96 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.CompilerServices;
 
 
 public class Scripture
 {
+    private Reference _reference ;
+    private Random _random = new Random ();
+
+    private  List<Word> _words = new List<Word>();
+
     
+
+   public Scripture(Reference reference , string text)
+    {
+        _reference = reference;
+         
+
+       string [] parts = text.Split(" ");
+
+       foreach(string part in parts)
+        {
+            Word word = new Word(part);
+            _words.Add(word);
+        }
+
+    
+       
+    }
+
+    public void HideRandomWords (int numberToHide)
+    {
+
+
+
+
+            int hiddenCount = 0;
+            while (hiddenCount<numberToHide)
+            {
+                int index = _random.Next(_words.Count);
+
+                Word word = _words[index];
+
+
+
+
+                if(!_words[index].IsHidden())
+                {
+                    _words[index].Hide();
+                    hiddenCount++;
+                }
+
+                if (IsCompletelyHidden())
+                {
+                    break;
+                }
+            
+            
+            }
+     
+
+     
+       
+    }
+
+    public string GetDisplayText()
+    {
+        string display = _reference.GetDisplayText() + " ";
+
+        foreach (Word word in _words)
+        {
+            display = display + word.GetDisplayText() + " ";
+        }
+
+        return display;
+    }
+
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+           if(!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 
     
 }
