8cab081a9e0d577667648d061c6fbefd6c0ea262
Jaures Guedou <sewlanguedou@gmail.com>
2026-06-02 10:30:07 +0100

    I submit the Homework

---
+++ week05/Homework/MathAssignment.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+public class MathAssignment : Assignment
+{
+    private string _textbookSection = "";
+    private string _problems = "";
+
+    public MathAssignment (string studentName,string topic,string textbookSection,string problems) : base(studentName,topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+
+    public string GetHomeworkList ()
+    {
+        return $"{_textbookSection} {_problems}";
+    }
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment assignment = new Assignment("Samuel Bennett","Multiplication");
+
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez","Fractions","Section 7.3","Problems 8-19");
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters","European History","The Causes of World War 2");
+
+        string  summary = assignment.GetSummary();
+
+        string   summary1 = mathAssignment.GetSummary();
+        string   Homework = mathAssignment.GetHomeworkList();
+
+        string   summary2 = writingAssignment.GetSummary();
+        string   Homework2 = writingAssignment.GetWritingInformation();
+        Console.WriteLine(summary);
+        Console.WriteLine();
+        Console.WriteLine(summary1);
+        Console.WriteLine(Homework);
+        Console.WriteLine();
+        Console.WriteLine(summary2);
+        Console.WriteLine(Homework2);
+
+
     }
 }
